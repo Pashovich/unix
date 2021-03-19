9cafa36ad0f1a341216d4f15f20fc6167cb76bac
p.sidorov <addons125@gmail.com>
2021-03-19 17:39:02 +0700

    1 and 2

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # unix
+
+## Lab1 
+* В директории lab1 `./script.sh`
+
+
+## Lab2
+* В директории lab2 `g++ main.cpp -o main -pthread`
---
+++ lab1/script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# check if there is an arg with filename
+if [ -z "$1" ]; then
+  echo filename not in args
+  exit 1
+fi
+
+FILENAME_STRING="$(grep -i "Output:" "$1")"
+FILENAME_WITH_WHITESPACE=${FILENAME_STRING#//Output:}
+FILENAME=$(echo "$FILENAME_WITH_WHITESPACE" | tr -d '[:space:]')
+
+# check if var FILENAME is not empty and contains name
+if [ -z "$FILENAME" ]; then
+  echo File name is not valid
+  exit 1
+fi
+
+TEMP_FOLDER=$(mktemp -d)
+
+# check if program was killed to remove temp dir
+trap "rm -rf TEMP_FOLDER; exit 1" SIGKILL SIGINT SIGHUP SIGTERM SIGQUIT
+
+cp "$1" "$TEMP_FOLDER"/
+
+PRIMARY_DIR=$(pwd)
+
+cd $TEMP_FOLDER
+gcc "$1" -o "$FILENAME"
+
+# check if compilation succeded 
+if [ $? -ne 0 ]; then
+  echo Compilation failed
+  cd "$PRIMARY_DIR"
+  rm -rf "$TEMP_FOLDER"
+  exit 1
+fi
+
+echo Compilated: $FILENAME
+#delete temp dir
+mv "$FILENAME" "$PRIMARY_DIR"
+cd "$PRIMARY_DIR"
+rm -rf "$TEMP_FOLDER"
