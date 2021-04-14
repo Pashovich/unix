c928c95696692a8a6e6aa69aa75e54fefeaf99d1
p.sidorov <addons125@gmail.com>
2021-04-14 15:02:57 +0700

    c

--- lab1/main.c
+++ lab1/main.c
@@ -6,4 +6,4 @@ int main()
     return 0;
 }
 
-//Output:      program
+//Output:  program
--- lab1/script.sh
+++ lab1/script.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # check if there is an arg with filename
-if [ -z "$1" ]; then
+if [ -z "$1" ] 
+then
   echo filename not in args
   exit 1
 fi
 
-FILENAME_STRING="$(grep -i "Output:" "$1")"
+
+FILENAME_STRING="$(grep -i "output:" "$1")"
 FILENAME_WITH_WHITESPACE=${FILENAME_STRING#//Output:}
-FILENAME=$(echo "$FILENAME_WITH_WHITESPACE" | tr -d '[:space:]')
+FILENAME=$(echo "$FILENAME_WITH_WHITESPACE" | tr -d [:space:])
 
 # check if var FILENAME is not empty and contains name
-if [ -z "$FILENAME" ]; then
+if [ -z "$FILENAME" ] 
+then
   echo File name is not valid
   exit 1
 fi
@@ -19,7 +22,7 @@ fi
 TEMP_FOLDER=$(mktemp -d)
 
 # check if program was killed to remove temp dir
-trap "rm -rf TEMP_FOLDER; exit 1" SIGKILL SIGINT SIGHUP SIGTERM SIGQUIT
+trap "rm -rf TEMP_FOLDER; exit 1" SIGINT SIGHUP SIGTERM 
 
 cp "$1" "$TEMP_FOLDER"/
 
@@ -29,7 +32,8 @@ cd $TEMP_FOLDER
 gcc "$1" -o "$FILENAME"
 
 # check if compilation succeded 
-if [ $? -ne 0 ]; then
+if [ $? -ne 0 ] 
+then
   echo Compilation failed
   cd "$PRIMARY_DIR"
   rm -rf "$TEMP_FOLDER"
